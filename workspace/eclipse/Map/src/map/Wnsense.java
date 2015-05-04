9cc13c45c4345fef6721dbf7aeb73a043d1892f0
sinorr <cynorr@sina.com>
2015-05-04 20:23:33 +0800

    mapStruct

---
+++ workspace/eclipse/Map/src/map/Wnsense.java
@@ -0,0 +1,27 @@
+package map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+
+public class Wnsense{
+	
+	HashSet<String> stops;
+	HashMap<String,Double[]> word2vec;
+	HashMap<String,Integer[]> index;
+	String[] lines;
+	Double[][] id2vec;
+	int[] flag;
+	
+	/////////////////////Methods//////////////////
+	
+	// Constructor
+	public Wnsense(){
+		
+	}
+	
+	public Entry<Integer[],Double[][]> getWn(String word){
+		
+		return null;
+	}
+}
---
+++ workspace/word2vec-master/run.sh
@@ -0,0 +1,1 @@
+./word2vec -train large2.en -output vectors.txt -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 0
